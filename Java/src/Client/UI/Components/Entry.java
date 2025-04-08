7e4010f818b5c81adcf411c9d5d77c3c87b5ed01
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2025-04-08 12:01:26 +0200

    Fix hovering

--- Java/src/Client/UI/Components/Entry.java
+++ Java/src/Client/UI/Components/Entry.java
@@ -13,8 +13,12 @@ package Client.UI.Components;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import Client.ICallback;
@@ -22,16 +26,20 @@ import Client.ICallback;
 @SuppressWarnings("serial")
 public class Entry extends JPanel {
 	private boolean hover		= true;
+	private boolean hovering	= false;
+	private boolean clicking	= false;
 	private String name			= null;
 	private String text			= null;
 	private ICallback callback	= null;
+	private JLabel label		= null;
 	
 	public Entry(String name, String text) {
-		this.name = name;
-		this.text = text;
-		
+		this.name		= name;
+		this.text		= text;
+		this.label		= new JLabel(this.text);
+
+		this.add(this.label);
 		this.setLayout(new FlowLayout(FlowLayout.LEFT));
-		this.add(new JLabel(this.text));
 		this.addMouseListener(new MouseAdapter() {
 			@Override
             public void mouseClicked(MouseEvent e) {
@@ -43,33 +51,55 @@ public class Entry extends JPanel {
 			@Override
             public void mouseEntered(MouseEvent e) {
                 if(hover) {
-                	setBackground(Color.CYAN);
+                	hovering = true;
             		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            		repaint();
                 }
             }
 			
 			@Override
             public void mouseExited(MouseEvent e) {
                 if(hover) {
-                	setBackground(null);
+                	hovering = false;
+					label.setForeground(null);
+            		repaint();
                 }
             }
 			
 			@Override
             public void mousePressed(MouseEvent e) {
 				if(hover) {
-					setBackground(Color.GREEN);
+					clicking = true;
+					label.setForeground(Color.white);
+            		repaint();
 				}
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
             	if(hover) {
-            		setBackground(null);
+            		clicking = false;
+					label.setForeground(null);
+            		repaint();
             	}
             }
         });
 	}
+	
+	@Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        if(hovering) {
+        	if(clicking) {
+        		g.setColor(new Color(0, 0, 128, 204));
+        	} else {
+        		g.setColor(new Color(255, 255, 255, 77));
+        	}
+        	
+            g.fillRect(0, 0, getWidth(), getHeight());
+        }
+    }
 	
 	public String getName() {
 		return this.name;
